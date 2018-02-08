dac9cf1d181db029781940d96b82c5737ee29337
decordoba <danieldecordobagil@hotmail.com>
2018-02-08 15:52:29 -0600

    Added atom configuration (optimized for python)

---
+++ .atom/additional_commands.sh
@@ -0,0 +1,6 @@
+# Additional commands necessary for the packages to word as expected
+sudo pip3 install flake8
+sudo pip3 install flake8-docstrings
+sudo pip3 install autopep8
+sudo pip3 install isort
+
