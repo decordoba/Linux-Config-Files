33bbf1dd9c2b579b3f8ff389c66bc71c5903fd4f
decordoba <danieldecordobagil@hotmail.com>
2018-02-08 18:28:33 -0600

    flake8 substitued by flake8-pep257 to fix D103 error always showing

--- .atom/additional_commands.sh
+++ .atom/additional_commands.sh
@@ -1,5 +1,5 @@
 # Additional commands necessary for the packages to word as expected
-sudo pip3 install flake8
+sudo pip3 install flake8-pep257
 sudo pip3 install flake8-docstrings
 sudo pip3 install autopep8
 sudo pip3 install isort
